9fc4a305a30802523b8f37f625331e33f0cb1ed6
Emanuele Grasso <96300448+L0P0P@users.noreply.github.com>
2022-09-03 10:55:33 +0200

    Aggiunta sezione mappa e OpenData

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.get('/', (req, res) => {
   res.render(__dirname + '/views/index.html');
 });
 
+// ----- Mappa: elaborazione lato server -----
+app.get('/map', (req, res) => {
+  res.render(__dirname + '/views/map.html');
+});
+
+app.get('/rawdata', (req, res) => {
+  console.log("Richiesta dei dati puri.");
+  res.type("text/csv").sendFile(__dirname + "/views/data.csv");
+});
+
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
